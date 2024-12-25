0d32e9ef1a408c5e1308c189f8b458191256897d
Josue Jimenez <josuejimenez20@gmail.com>
2024-12-25 16:13:02 -0500

    Add product as Admin redesign product

--- frontend/src/components/ultimateUser/UltimateUser.jsx
+++ frontend/src/components/ultimateUser/UltimateUser.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { Stack } from '@mui/material';
 import { UltimateUserRouter } from "../../router/ultimateUser/UltimateUserRouter";
 import { CustomButtonNavigate } from "../shared/atoms/CustomButtonNavigate";
 
 export function UltimateUser() {
-    return (<>
-        <div className='containerTopNavBar'>
-            <CustomButtonNavigate label='Agregar Productos' linkTo='AddProducts' />
-            <CustomButtonNavigate label='Editar Producto' linkTo='AddProducts' />
-            <CustomButtonNavigate label='Administracion de Usuarios' linkTo='AddProducts' />
-        </div>
-        <UltimateUserRouter />
-    </>);
+    return (
+        <>
+            <Stack 
+                direction="row" 
+                spacing={4} // Espaciado entre los botones
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center', // Centra horizontalmente
+                    alignItems: 'center',    // Alinea verticalmente
+                    mt: 4,  
+                    marginBottom: '1em'                // Espaciado superior opcional
+                }}
+            >
+                <CustomButtonNavigate label='Agregar Productos' linkTo='AddProducts' />
+                <CustomButtonNavigate label='Editar Producto' linkTo='EditProduct' />
+                <CustomButtonNavigate label='Administración de Usuarios' linkTo='UserManagement' />
+            </Stack>
+            <UltimateUserRouter />
+        </>
+    );
 }
